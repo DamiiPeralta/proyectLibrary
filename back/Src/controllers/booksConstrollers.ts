fa5fc10457b74987982ba38631d70422ee58bf26
Carlos Felice <carlosfelicelxn@gmail.com>
2024-03-27 15:24:13 -0300

    controllers and services

---
+++ back/Src/controllers/booksConstrollers.ts
@@ -0,0 +1,25 @@
+const {
+  getBooksService,
+  postBooksService,
+} = require("../services/booksService");
+
+const getBooksControllers = async (req, res) => {
+  const books = await getBooksService();
+  res.status(200).json(books);
+};
+const book : String = 'book';
+const contador :  Number = 1;
+
+const postBookController = async (req, res) => {
+  try {
+    const bookData = req.body;
+    console.log("Datos recibidos", bookData);
+    await postBooksService(bookData);
+    res.status(201).json({ error: false, data: bookData });
+  } catch (error) {
+    console.log("Error al procesar la solicitud: ", error);
+    res.status(500).json({ error: true, message: error.message });
+  }
+};
+
+module.exports = { getBooksControllers, postBookController };
