4c61c4c202c336a823c831ba0246937346dfa4aa
alejandro0419 <alejandrocampaya@hotmail.com>
2024-03-29 02:05:59 -0300

    cambios menores

--- front/scripts/search.js
+++ front/scripts/search.js
@@ -12,19 +12,14 @@ function filterBooks(searchTerm, books) {
     const title = book.title.toLowerCase();
     const author = book.author.toLowerCase();
     const genre = book.genre.map((genre) => genre.toLowerCase());
-    console.log('antes del if');
     if(title.includes(searchTerm.toLowerCase()) ||      author.includes(searchTerm.toLowerCase()) || genre.includes(searchTerm.toLowerCase())){
-      console.log('en el if');
       filteredBooks.push(book);
-      console.log('despues del push');
     };
 
   });
-  console.log(filteredBooks);
   return filteredBooks;
 
 } 
-
 
 const search = async () => {
   const searchTerm = document.getElementById("searchInput").value;
@@ -33,7 +28,6 @@ const search = async () => {
     const { data } = await axios("http://localhost:3000/libros");
     const booksToFilter = data;
     const filteredBooks = filterBooks(searchTerm, booksToFilter);
-    //document.getElementById('cardsContainer').innerHTML='';
     render(filteredBooks);
     
   } catch (error) {
@@ -41,11 +35,5 @@ const search = async () => {
   }
 };
 
-
-/* 
-function search() {
-  const booksToFilter = getBooks();
-  console.log("Se esta guardando antes de buscar.");
-} */
 
 module.exports = search;
